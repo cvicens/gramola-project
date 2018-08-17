d4e17074197099834a42fe412a3f51d104421501
Carlos Vicens <carlos.vicens.alonso@gmail.com>
2018-08-17 11:19:18 +0200

    updated data

--- events/src/main/resources/import.sql
+++ events/src/main/resources/import.sql
@@ -2,6 +2,5 @@ insert into fruit (name) values ('Cherry');
 insert into fruit (name) values ('Apple');
 insert into fruit (name) values ('Banana');
 
-insert into event (name, address, city, province, country, date, start_time, end_time, location, artist, description, image) values ('Guns n Roses 2018', 'Calle Alcalá 1', 'MADRID', 'MADRID', 'SPAIN', '2018-07-05', '18:00', '23:00', 'Plaza de toros de la Ventas', 'Guns n Roses', 'Lorem ipsum...', 'guns-P1080795.jpg');
-	
-insert into event (name, address, city, province, country, date, start_time, end_time, location, artist, description, image) values ('Foo Fighters', 'Calle Mayor 1', 'MADRID', 'MADRID', 'SPAIN', '2018-07-15', '18:00', '23:00', 'Plaza Mayor', 'Foo Fighters', 'Lorem ipsum...', 'foo-P1000628.jpg');
+insert into event (name, address, city, province, country, date, start_time, end_time, location, artist, description, image) values ('Not in the Lifetime Tour', 'Cmo. de Perales, 23, 28041', 'MADRID', 'MADRID', 'SPAIN', '2018-08-05', '18:00', '23:00', 'Caja Magica', 'Guns n Roses', 'The revived Guns N’ Roses and ...', 'guns-P1080795.jpg');
+insert into event (name, address, city, province, country, date, start_time, end_time, location, artist, description, image) values ('CONCRETE AND GOLD TOUR 2018', '8 Boulevard de Bercy, Paris', 'PARIS', 'PARIS', 'FRANCE', '2018-09-15', '18:00', '23:00', 'AccorHotels Arena', 'Foo Fighters', 'Concrete and Gold Tour is...', 'foo-P1000628.jpg');
